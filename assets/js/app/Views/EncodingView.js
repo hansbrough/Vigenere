0141477251549e9d25cc39d9801eb9e7eb3dcca9
Hans Brough <hans.brough@yahoo.com>
2017-08-25 09:46:35 -0700

    keywords should be a single uppercase word between 3 and 8 characters
    + some ui niceities

--- assets/js/app/Views/EncodingView.js
+++ assets/js/app/Views/EncodingView.js
@@ -19,14 +19,13 @@ define([
 
     let _View = {
       init() {
-        //console.log("EncodingView"," init");
         this.el           = document.getElementById(CSS_PARENT);
         this.charTable    = this.el.querySelector(CSS_BTNS);
         this.encodedDisplay = this.el.querySelector(CSS_E_TXT);
         this.sourceDisplay  = this.el.querySelector(CSS_S_TXT);
         this.clearInput   = this.el.querySelector(CSS_CLEAR);
-
         this.template     = Handlebars.compile(Template);
+        //listen for pertinent evts from other objects
         PubSub.subscribe('dictionary:encoding:updated', this.render.bind(this) );
         PubSub.subscribe('source:character:encoded', this.appendEncodedText.bind(this));
         PubSub.subscribe('cipher:update', this.clearFields.bind(this));
@@ -34,12 +33,10 @@ define([
         this.delegateEvts();
       },
       appendEncodedText(char){
-        //console.log("EncodingView"," appendEncodedText:",char);
         let val = this.encodedDisplay.innerText;
         this.encodedDisplay.innerText = val+char;
       },
       appendSourceText(char){
-        //let val = this.sourceDisplay.value;
         let val = this.sourceDisplay.innerText;
         this.sourceDisplay.innerText = val+char;
       },
@@ -48,7 +45,6 @@ define([
         this.encodedDisplay.innerText = '';
       },
       delegateEvts(){
-        //console.log("EncodingView"," delegateEvts:");
         this.charTable.addEventListener('click',function(e){
           e.preventDefault();
           if(RE_CHAR_BTN.test(e.target.nodeName)){
@@ -61,7 +57,6 @@ define([
         this.clearInput.addEventListener('click', this.clearFields.bind(this));
       },
       render(dict=[]) {
-        //console.log("EncodingView"," dict: ", dict);
         this.el.querySelector(CSS_E_DICT).innerHTML = this.template(dict);
       }
     }
--- assets/js/app/Views/KeywordView.js
+++ assets/js/app/Views/KeywordView.js
@@ -8,44 +8,58 @@ define([
 ],
   function (PubSub, Handlebars, Template) {
     //note: some or all of these constants could be passed in to view as part of a configuration
-    const RE_UPDATE_BTN = /update-keyword/;
-    const CSS_PARENT    = 'keyword_configuration';
-    const CSS_ENTRY     = '.keyword-entry';
-    const CSS_TABLE     = '.keyword-offset-table';
-    const CSS_CHARS     = '.keyword-chars';
-    const CSS_OFFSET    = '.offset-chars';
-    const CSS_ACTIVE    = 'active';
-    const CSS_ROW       = 'row';
-    const CSS_ERR       = 'error';
+    const RE_UPDATE_BTN   = /update-keyword/;
+    const RE_ENTRY        = /keyword-entry/;
+    const RE_SANITIZE     = /[^a-z+]+/gi;
+    const CSS_UPDATE_BTN  = '.update-keyword';
+    const CSS_PARENT      = 'keyword_configuration';
+    const CSS_ENTRY       = '.keyword-entry';
+    const CSS_TABLE       = '.keyword-offset-table';
+    const CSS_CHARS       = '.keyword-chars';
+    const CSS_OFFSET      = '.offset-chars';
+    const CSS_ACTIVE      = 'active';
+    const CSS_DISABLED    = 'disabled';
+    const CSS_ROW         = 'row';
+    const CSS_ERR         = 'error';
 
     let _View = {
       init() {
-        //console.log("KeywordView"," init");
-        this.el = document.getElementById(CSS_PARENT);
-        this.input = this.el.querySelector(CSS_ENTRY);
+        this.el     = document.getElementById(CSS_PARENT);
+        this.input  = this.el.querySelector(CSS_ENTRY);
+        this.btn    = this.el.querySelector(CSS_UPDATE_BTN);
         this.template = Handlebars.compile(Template);
+        //listen for pertinent evts from other objects
         PubSub.subscribe('cipher:update', this.render.bind(this) );
         PubSub.subscribe('keyword:offset_index:update', (idx) => this.updateActive(idx));
 
         this.delegateEvts();
       },
       delegateEvts(){
-        //console.log("delegateEvts:");
         this.el.addEventListener('click',function(e){
           e.preventDefault();
-          let keyword = this.input.value.replace(/[^a-z+]+/gi,'');
+          let keyword = this.input.value.replace(RE_SANITIZE,'');
           if(RE_UPDATE_BTN.test(e.target.className)){
-            if(keyword !== ''){
-              this.el.classList.remove(CSS_ERR);
+            if(this.validate(keyword)){
+              this.input.value = keyword.toUpperCase();
               PubSub.publish('keyword:update',keyword);
+            }
+          }
+        }.bind(this),false);
+
+        this.el.addEventListener('keyup',function(e){
+          let keyword = this.input.value.replace(RE_SANITIZE,'');
+          if(RE_ENTRY.test(e.target.className)){
+            if(this.validate(keyword)){
+              this.el.classList.remove(CSS_ERR);
+              this.btn.classList.remove(CSS_DISABLED);
             }else{
               this.el.classList.add(CSS_ERR);
+              this.btn.classList.add(CSS_DISABLED);
             }
           }
         }.bind(this),false);
       },
       render(cipher={}) {
-        //console.log("KeywordView"," cipher: ", cipher);
         if( !Array.isArray(cipher.keyword) ){
           cipher.keyword = cipher.keyword.split('');
         }
@@ -53,7 +67,6 @@ define([
         this.updateActive(0);
       },
       updateActive(idx){
-        //console.log("KeywordView"," update:",idx);
         //unset any previously active child nodes in both rows.
         [...this.el.getElementsByClassName(CSS_ROW)].forEach(
           (node) => {
@@ -62,6 +75,10 @@ define([
         );
         this.el.querySelector(CSS_CHARS).childNodes[idx].classList.add(CSS_ACTIVE);
         this.el.querySelector(CSS_OFFSET).childNodes[idx].classList.add(CSS_ACTIVE);
+      },
+      validate(str){
+        let len = str.length;
+        return (len >= 3 && len <= 8);
       }
     }
 
